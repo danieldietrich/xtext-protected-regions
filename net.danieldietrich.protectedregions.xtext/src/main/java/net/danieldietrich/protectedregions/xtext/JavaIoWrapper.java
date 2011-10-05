1b778225a89f9c6f4aac09abe53c078e312384c8
Hendy Irawan <hendy@soluvas.com>
2011-10-06 01:54:03 +0700

    Workaround for https://github.com/danieldietrich/xtext-protectedregions/issues/21. Fixed #21.
    However it is very hackish. Let me know if you have a better idea.
    The structure is now a JavaIoWrapper that wraps a ProtectedRegionSupport
    that wraps a BidiJavaIoWrapper. Strange structure.
    
    Saentity sample project has been updated.

--- net.danieldietrich.protectedregions.xtext/src/main/java/net/danieldietrich/protectedregions/xtext/JavaIoWrapper.java
+++ net.danieldietrich.protectedregions.xtext/src/main/java/net/danieldietrich/protectedregions/xtext/JavaIoWrapper.java
@@ -3,7 +3,13 @@
  */
 package net.danieldietrich.protectedregions.xtext;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.eclipse.xtext.generator.AbstractFileSystemAccess;
 import org.eclipse.xtext.generator.JavaIoFileSystemAccess;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Wraps a {@link ProtectedRegionSupport} so it can be Guice-injected as a {@link JavaIoFileSystemAccess}.
@@ -14,14 +20,25 @@ import org.eclipse.xtext.generator.JavaIoFileSystemAccess;
  */
 public class JavaIoWrapper extends JavaIoFileSystemAccess {
 
+  private final transient Logger logger = LoggerFactory.getLogger(JavaIoWrapper.class); 
+  IProtectedRegionSupportFactory prsFactory;
   ProtectedRegionSupport prs;
   
-  public JavaIoWrapper(ProtectedRegionSupport prs) {
-    this.prs = prs;
+  public JavaIoWrapper(IProtectedRegionSupportFactory prsFactory) {
+    this.prsFactory = prsFactory;
   }
   
   @Override
   public void generateFile(String fileName, String slot, CharSequence contents) {
+    if (prs == null) {
+      Map<String, String> pathes = getPathes();
+      logger.debug("Creating ProtectedRegionSupport with slots: {}", pathes);
+      IBidiFileSystemAccess delegate = prsFactory.createFileSystemAccess();
+      for (Entry<String, String> path : pathes.entrySet()) {
+        delegate.setOutputPath(path.getKey(), path.getValue());
+      }
+      this.prs = prsFactory.createProtectedRegionSupport(delegate);
+    }
     prs.generateFile(fileName, slot, contents);
   }
   
@@ -30,14 +47,14 @@ public class JavaIoWrapper extends JavaIoFileSystemAccess {
     prs.deleteFile(fileName);
   }
 
-  @Override
-  public void setOutputPath(String path) {
-    prs.setOutputPath(DEFAULT_OUTPUT, path);
-  }
-  
-  @Override
-  public void setOutputPath(String outputName, String path) {
-    prs.setOutputPath(outputName, path);
-  }
+//  @Override
+//  public void setOutputPath(String path) {
+//    prs.setOutputPath(DEFAULT_OUTPUT, path);
+//  }
+//  
+//  @Override
+//  public void setOutputPath(String outputName, String path) {
+//    prs.setOutputPath(outputName, path);
+//  }
 
 }
