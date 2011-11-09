6d4ab0ac9775eae51e37519de8478a204e65960f
Daniel Dietrich <cafebab3@gmail.com>
2011-11-09 21:27:16 +0100

    Refs #28. Added missing Xtext 2.1 API. (AbstractFileSystemAccess.setOutputConfigurations(Map<String,OutputConfiguration> outputs))

--- plugins/net.danieldietrich.protectedregions.xtext/src/main/java/net/danieldietrich/protectedregions/xtext/BidiJavaIoFileSystemAccess.java
+++ plugins/net.danieldietrich.protectedregions.xtext/src/main/java/net/danieldietrich/protectedregions/xtext/BidiJavaIoFileSystemAccess.java
@@ -14,6 +14,7 @@ import net.danieldietrich.protectedregions.support.IProtectedRegionSupport;
 
 import org.apache.commons.io.FileUtils;
 import org.eclipse.xtext.generator.JavaIoFileSystemAccess;
+import org.eclipse.xtext.generator.OutputConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,116 +24,130 @@ import org.slf4j.LoggerFactory;
  */
 public class BidiJavaIoFileSystemAccess extends JavaIoFileSystemAccess implements IFileSystemReader {
 
-  private transient Logger logger = LoggerFactory.getLogger(BidiJavaIoFileSystemAccess.class);
+	private transient Logger logger = LoggerFactory.getLogger(BidiJavaIoFileSystemAccess.class);
 
-  private final IProtectedRegionSupport support;
-  private IPathFilter filter;
+	private final IProtectedRegionSupport support;
+	private IPathFilter filter;
 
-  public BidiJavaIoFileSystemAccess(IProtectedRegionSupport support) {
-    this.support = support;
-  }
+	public BidiJavaIoFileSystemAccess(IProtectedRegionSupport support) {
+		this.support = support;
+	}
 
-  protected IProtectedRegionSupport getSupport() {
-    return support;
-  }
+	protected IProtectedRegionSupport getSupport() {
+		return support;
+	}
 
-  @Override
-  public void setOutputPath(String path) {
-    setOutputPath(DEFAULT_OUTPUT, path);
-  }
+	@Override
+	public void setOutputConfigurations(Map<String, OutputConfiguration> outputs) {
+		super.setOutputConfigurations(outputs);
+		if (outputs != null) {
+			for (OutputConfiguration output : outputs.values()) {
+				readRegions(output.getName(), output.getOutputDirectory());
+			}
+		}
+	}
+	
+	@Override
+	public void setOutputPath(String path) {
+		setOutputPath(DEFAULT_OUTPUT, path);
+	}
 
-  @Override
-  public void setOutputPath(String outputName, String path) {
-    super.setOutputPath(outputName, path);
-    logger.info("Adding slot {} at {}", path, outputName);
-    support.readRegions(this, outputName);
-  }
+	@Override
+	public void setOutputPath(String outputName, String path) {
+		super.setOutputPath(outputName, path);
+		readRegions(outputName, path);
+	}
 
-  @Override
-  public void generateFile(String fileName, CharSequence contents) {
-    URI uri = getUri(fileName);
-    logger.debug("Generating {} at {} => {}", new Object[] {fileName, DEFAULT_OUTPUT, uri});
-    CharSequence mergedContents = support.mergeRegions(this, fileName, DEFAULT_OUTPUT, contents);
-    super.generateFile(fileName, mergedContents);
-  }
+	@Override
+	public void generateFile(String fileName, CharSequence contents) {
+		URI uri = getUri(fileName);
+		logger.debug("Generating {} at {} => {}", new Object[] { fileName, DEFAULT_OUTPUT, uri });
+		CharSequence mergedContents = support.mergeRegions(this, fileName, DEFAULT_OUTPUT, contents);
+		super.generateFile(fileName, mergedContents);
+	}
 
-  @Override
-  public void generateFile(String fileName, String slot, CharSequence contents) {
-    URI uri = getUri(fileName, slot);
-    logger.debug("Generating {} at {} => {}", new Object[] {fileName, slot, uri});
-    CharSequence mergedContents = support.mergeRegions(this, fileName, slot, contents);
-    super.generateFile(fileName, slot, mergedContents);
-  }
+	@Override
+	public void generateFile(String fileName, String slot, CharSequence contents) {
+		URI uri = getUri(fileName, slot);
+		logger.debug("Generating {} at {} => {}", new Object[] { fileName, slot, uri });
+		CharSequence mergedContents = support.mergeRegions(this, fileName, slot, contents);
+		super.generateFile(fileName, slot, mergedContents);
+	}
 
-  //@Override
-  public IPathFilter getFilter() {
-    return filter;
-  }
+	//@Override
+	public IPathFilter getFilter() {
+		return filter;
+	}
 
-  //@Override
-  public void setFilter(IPathFilter filter) {
-    this.filter = filter;
-  }
+	//@Override
+	public void setFilter(IPathFilter filter) {
+		this.filter = filter;
+	}
 
-  //@Override
-  public boolean exists(URI uri) {
-    return new File(uri).exists();
-  }
+	//@Override
+	public boolean exists(URI uri) {
+		return new File(uri).exists();
+	}
 
-  //@Override
-  public CharSequence readFile(URI uri) throws IllegalArgumentException, IOException {
-    final File file = new File(uri);
-    return FileUtils.readFileToString(file);
-  }
+	//@Override
+	public CharSequence readFile(URI uri) throws IllegalArgumentException, IOException {
+		final File file = new File(uri);
+		return FileUtils.readFileToString(file);
+	}
 
-  //@Override
-  public Set<URI> listFiles(URI path) {
-    Collection<File> files = FileUtils.listFiles(new File(path), null, true);
-    Set<URI> result = new HashSet<URI>();
-    for (File file : files) {
-      if (filter == null || filter.accept(file.toURI())) {
-        result.add(file.toURI());
-      }
-    }
-    return result;
-  }
+	//@Override
+	public Set<URI> listFiles(URI path) {
+		Collection<File> files = FileUtils.listFiles(new File(path), null, true);
+		Set<URI> result = new HashSet<URI>();
+		for (File file : files) {
+			if (filter == null || filter.accept(file.toURI())) {
+				result.add(file.toURI());
+			}
+		}
+		return result;
+	}
 
-  //@Override
-  public boolean hasFiles(URI uri) {
-    return new File(uri).isDirectory();
-  }
+	//@Override
+	public boolean hasFiles(URI uri) {
+		return new File(uri).isDirectory();
+	}
 
-  //@Override
-  public boolean isFile(URI uri) {
-    return new File(uri).isFile();
-  }
+	//@Override
+	public boolean isFile(URI uri) {
+		return new File(uri).isFile();
+	}
 
-  //@Override
-  public String getCanonicalPath(URI uri) {
-    try {
-      return new File(uri).getCanonicalPath();
-    } catch (IOException e) {
-      logger.warn("Cannot get canonical path for {}.", uri);
-      return uri.getRawPath();
-    }
-  }
+	//@Override
+	public String getCanonicalPath(URI uri) {
+		try {
+			return new File(uri).getCanonicalPath();
+		} catch (IOException e) {
+			logger.warn("Cannot get canonical path for {}.", uri);
+			return uri.getRawPath();
+		}
+	}
 
-  //@Override
-  public URI getUri(String relativePath) {
-    return getUri(relativePath, DEFAULT_OUTPUT);
-  }
+	//@Override
+	public URI getUri(String relativePath) {
+		return getUri(relativePath, DEFAULT_OUTPUT);
+	}
 
-  //@Override
-  public URI getUri(String relativePath, String slot) {
-    Map<String, String> pathes = getPathes();
-    if (pathes.size() == 0) {
-      throw new IllegalStateException("No slots initialized!? Call #setOutputPath(...)");
-    }
-    String slotPath = pathes.get((slot == null) ? DEFAULT_OUTPUT : slot);
-    if (slotPath == null) {
-      throw new IllegalStateException("Slot " + slot + " not found.");
-    }
-    return new File(slotPath + "/" + relativePath).toURI();
-  }
+	//@Override
+	public URI getUri(String relativePath, String slot) {
+		Map<String, String> pathes = getPathes();
+		if (pathes.size() == 0) {
+			throw new IllegalStateException("No slots initialized!? Call #setOutputPath(...)");
+		}
+		String slotPath = pathes.get((slot == null) ? DEFAULT_OUTPUT : slot);
+		if (slotPath == null) {
+			throw new IllegalStateException("Slot " + slot + " not found.");
+		}
+		return new File(slotPath + "/" + relativePath).toURI();
+	}
+	
+	private void readRegions(String slot, String path) {
+		logger.info("Adding slot {} => {}", slot, path);
+		support.readRegions(this, slot);
+	}
 
 }
